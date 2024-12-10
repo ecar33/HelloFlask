9373324dd64e9da9d18a0f51359da0a450064282
Evan Carlile <evanc2022@gmail.com>
2024-12-10 01:11:32 -0600

    Rendering, templates, inheritance, variables + expressions

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, url_for, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User page for: {escape(name)}'
+
+@app.route('/movies')
+def test_url_for():
+    return render_template('movies.html', name=name, movies=movies)
+
+
+name = 'Evan Carlile'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
